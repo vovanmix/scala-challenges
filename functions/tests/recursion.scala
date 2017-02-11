b37bb614af2fefd5a066a03e59d1e1c20b66029d
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2017-02-11 12:57:51 -0800

    Create recursion.scala

---
+++ functions/tests/recursion.scala
@@ -0,0 +1,67 @@
+package recfun
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class BalanceSuite extends FunSuite {
+  import Main.balance
+
+  test("balance: '(if (zero? x) max (/ 1 x))' is balanced") {
+    assert(balance("(if (zero? x) max (/ 1 x))".toList))
+  }
+
+  test("balance: 'I told him ...' is balanced") {
+    assert(balance("I told him (that it's not (yet) done).\n(But he wasn't listening)".toList))
+  }
+
+  test("balance: ':-)' is unbalanced") {
+    assert(!balance(":-)".toList))
+  }
+
+  test("balance: counting is not enough") {
+    assert(!balance("())(".toList))
+  }
+}
+
+@RunWith(classOf[JUnitRunner])
+class CountChangeSuite extends FunSuite {
+  import Main.countChange
+  test("countChange: example given in instructions") {
+    assert(countChange(4,List(1,2)) === 3)
+  }
+
+  test("countChange: sorted CHF") {
+    assert(countChange(300,List(5,10,20,50,100,200,500)) === 1022)
+  }
+
+  test("countChange: no pennies") {
+    assert(countChange(301,List(5,10,20,50,100,200,500)) === 0)
+  }
+
+  test("countChange: unsorted CHF") {
+    assert(countChange(300,List(500,5,50,100,20,200,10)) === 1022)
+  }
+
+  test("countChange: no coins") {
+    assert(countChange(300,List()) === 0)
+  }
+}
+
+@RunWith(classOf[JUnitRunner])
+class PascalSuite extends FunSuite {
+  import Main.pascal
+
+  test("pascal: col=0,row=2") {
+    assert(pascal(0,2) === 1)
+  }
+
+  test("pascal: col=1,row=2") {
+    assert(pascal(1,2) === 2)
+  }
+
+  test("pascal: col=1,row=3") {
+    assert(pascal(1,3) === 3)
+  }
+}
