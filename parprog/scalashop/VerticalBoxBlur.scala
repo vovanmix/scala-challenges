54db26eca73051ac21a6ea56f9ce0756479cfa74
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2017-02-11 20:42:47 -0800

    Create VerticalBoxBlur.scala

---
+++ parprog/scalashop/VerticalBoxBlur.scala
@@ -0,0 +1,73 @@
+package scalashop
+
+import java.util.concurrent.ForkJoinTask
+
+import org.scalameter._
+import common._
+
+object VerticalBoxBlurRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      VerticalBoxBlur.blur(src, dst, 0, width, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      VerticalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+
+}
+
+/** A simple, trivially parallelizable computation. */
+object VerticalBoxBlur {
+
+  /** Blurs the columns of the source image `src` into the destination image
+   *  `dst`, starting with `from` and ending with `end` (non-inclusive).
+   *
+   *  Within each column, `blur` traverses the pixels by going from top to
+   *  bottom.
+   */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    for(c <- from until end; r <- 0 until src.height) dst.update(c, r, boxBlurKernel(src, c, r, radius))
+  }
+
+  /** Blurs the columns of the source image in parallel using `numTasks` tasks.
+   *
+   *  Parallelization is done by stripping the source image `src` into
+   *  `numTasks` separate strips, where each strip is composed of some number of
+   *  columns.
+   */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    val num = math.ceil(src.width.toDouble / numTasks).toInt
+    val spl1: IndexedSeq[Int] = for(x <- 0 until src.width by num) yield x
+    val splPoints = spl1.zip(spl1.tail.toList ::: List(src.width))
+
+    var tasks: Array[ForkJoinTask[Unit]] = Array()
+
+    splPoints.foreach{ case (st, end) =>
+      tasks = tasks :+ task {
+        blur(src, dst, st, end, radius)
+      }
+    }
+
+    tasks.foreach{ (a) => a.join() }
+  }
+
+}
