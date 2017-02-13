a892c292cb666eb47206333334e5cee399fd5646
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2017-02-12 23:34:59 -0800

    Create ParallelCountChange.scala

---
+++ parprog/reductions/ParallelCountChange.scala
@@ -0,0 +1,84 @@
+package reductions
+
+import org.scalameter._
+import common._
+
+object ParallelCountChangeRunner {
+
+  @volatile var seqResult = 0
+
+  @volatile var parResult = 0
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 20,
+    Key.exec.maxWarmupRuns -> 40,
+    Key.exec.benchRuns -> 80,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val amount = 250
+    val coins = List(1, 2, 5, 10, 20, 50)
+    val seqtime = standardConfig measure {
+      seqResult = ParallelCountChange.countChange(amount, coins)
+    }
+    println(s"sequential result = $seqResult")
+    println(s"sequential count time: $seqtime ms")
+
+    def measureParallelCountChange(threshold: ParallelCountChange.Threshold): Unit = {
+      val fjtime = standardConfig measure {
+        parResult = ParallelCountChange.parCountChange(amount, coins, threshold)
+      }
+      println(s"$threshold parallel result = $parResult")
+      println(s"$threshold parallel count time: $fjtime ms")
+      println(s"$threshold speedup: ${seqtime / fjtime}")
+    }
+
+    measureParallelCountChange(ParallelCountChange.moneyThreshold(amount))
+    measureParallelCountChange(ParallelCountChange.totalCoinsThreshold(coins.length))
+    measureParallelCountChange(ParallelCountChange.combinedThreshold(amount, coins))
+  }
+}
+
+object ParallelCountChange {
+
+  /** Returns the number of ways change can be made from the specified list of
+   *  coins for the specified amount of money.
+   */
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (money == 0) 1
+    else if (money > 0 && coins.nonEmpty)
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    else 0
+
+  type Threshold = (Int, List[Int]) => Boolean
+
+  /** In parallel, counts the number of ways change can be made from the
+   *  specified list of coins for the specified amount of money.
+   */
+  def parCountChange(money: Int, coins: List[Int], threshold: Threshold): Int = {
+    if(money < 0 || coins.isEmpty || threshold(money, coins)) countChange(money, coins)
+    else {
+      val (l, r) = parallel[Int, Int](parCountChange(money - coins.head, coins, threshold),
+        parCountChange(money, coins.tail, threshold))
+      l + r
+    }
+  }
+
+  /** Threshold heuristic based on the starting money. */
+  def moneyThreshold(startingMoney: Int): Threshold = (money: Int, _: List[Int]) => money <= startingMoney * 2 / 3
+
+  /** Threshold heuristic based on the total number of initial coins. */
+  /** returns true when the number of coins is less than or equal to the 2 / 3 of the initial number of coins: */
+  def totalCoinsThreshold(totalCoins: Int): Threshold = (_: Int, coins: List[Int]) => coins.length <= totalCoins * 2 / 3
+
+
+  /** Threshold heuristic based on the starting money and the initial list of coins. */
+  /** returns true when the amount of money multiplied with the number of remaining coins
+      is less than or equal to the starting money multiplied with the initial number of coins divided by 2 */
+  def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = (money, coins) => {
+    val remaining = money * coins.length
+    val start = startingMoney * allCoins.length
+    remaining <= start / 2
+  }
+}
